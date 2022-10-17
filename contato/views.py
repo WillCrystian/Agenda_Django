7f83658eca1156617959e2966f981e2dc96faa91
willcrystian <willcrystian@gmail.com>
2022-10-17 15:45:09 -0300

    criado um campo de pesquisa

--- contato/views.py
+++ contato/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator
 from.models import Contato
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 def index(request):
     lista_contatos = Contato.objects.order_by('-id').filter(mostrar= True)
     
-    paginator = Paginator(lista_contatos, 3)
+    paginator = Paginator(lista_contatos, 2)
     paginas = request.GET.get('p')
     contatos = paginator.get_page(paginas)
     
@@ -20,4 +22,23 @@ def detalhes(request, contato_id):
         raise Http404()
     
     return render(request, 'detalhes.html', {'contato': contato})
+
+def busca(request):
+    termo = request.GET.get('termo')
+    if termo is None:
+        raise Http404()
+    
+    campos = Concat('nome', Value(' '), 'sobrenome')
+    
+    lista_contatos = Contato.objects.annotate(
+        nome_completo= campos
+    ).filter(
+        Q(nome_completo__icontains= termo) | Q(telefone__icontains= termo)
+        )  
+    
+    paginator = Paginator(lista_contatos, 2)
+    paginas = request.GET.get('p')
+    contatos = paginator.get_page(paginas)
+    
+    return render(request, 'busca.html', {'contatos': contatos})
     
